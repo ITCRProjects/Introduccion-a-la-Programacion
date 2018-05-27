f9fb60a25c5674a67aa7f3e44e4422456e71a567
rartaviasolano <37001107+rartaviasolano@users.noreply.github.com>
2018-05-27 13:04:42 -0600

    Add files via upload

---
+++ FiguraInicial.java
@@ -0,0 +1,27 @@
+public class FiguraInicial {
+    private int x;
+    private int y;
+
+    public int getX(int x){
+        return this.x;
+    }
+    public void setX(int x){
+        if ((x >= 0) & (x <= 1023)){
+            this.x = x;
+        }
+        else{
+            System.out.println("El valor de x debe ser mayor o igual a 0 y menor a 1024");
+        }
+    }
+    public int getY(int y){
+        return this.y;
+    }
+    public void setY(int y){
+        if ((y >= 0) & (x <= 768)){
+            this.y = y;
+        }
+        else{
+            System.out.println("El valor de y debe ser mayor o igual a 0 y menor a 769");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,49 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        CirculoInicial circ = new CirculoInicial();
+        RectanguloInicial rect = new RectanguloInicial();
+
+
+        System.out.println("*** Menú ***");
+        System.out.println("1. Calcular Area para Circulo.");
+        System.out.println("2. Calcular Area para Rectangulo.");
+        System.out.println("Ingrese la opción que desea ejecutar.");
+
+
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion = entrada.nextInt();
+        System.out.println("Ingrese el Ancho de la pantalla: ");
+        int valor1 = entrada.nextInt();
+        System.out.println("Ingrese el Alto de la pantalla: ");
+        int valor2 = entrada.nextInt();
+
+
+        double resultado = 0;
+
+        switch(opcion){
+            case 1:
+                System.out.println("Ingrese el Radio del Circulo: ");
+                int valor3 = entrada.nextInt();
+                resultado = circ.calcularArea(valor1, valor2, valor3);
+                break;
+            case 2:
+                System.out.println("Ingrese el Alto del Rectángulo: ");
+                int valor4 = entrada.nextInt();
+                System.out.println("Ingrese el Ancho del Rectángulo: ");
+                int valor5 = entrada.nextInt();
+                resultado = rect.calcularArea(valor1, valor2, valor4, valor5);
+                break;
+            default:
+                System.out.println("Default case");
+        }
+        System.out.println("El Area correspondiente es: " + resultado);
+
+    }
+}
