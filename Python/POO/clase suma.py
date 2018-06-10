c0e78311a1010cef5f4aca24a53a3f447af68647
Ricardo Artavia Solano <37001107+RicardoArtaviaSolano@users.noreply.github.com>
2018-06-09 22:09:50 -0600

    Add files via upload

---
+++ Python/POO/clase suma.py
@@ -0,0 +1,10 @@
+
+class Suma:
+    def __init__(self):
+        pass
+
+    def sumar_array(self, lista):
+        resultado = 0
+        for valor in lista:
+            resultado += valor
+        return resultado
